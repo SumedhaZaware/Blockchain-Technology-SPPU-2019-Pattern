64d5eaa20d330fbc00d3c50933bb927bd923312e
Sumedha Nitin Zaware <91329268+SumedhaZaware@users.noreply.github.com>
2022-11-23 10:04:59 +0530

    Update Assignment-3.sol

--- Assignment-3.sol
+++ Assignment-3.sol
@@ -1,1 +1,17 @@
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.7;
+contract Bank {
+   address public owner;
+   uint public balance;
+   constructor() {
+     owner = msg.sender;
+   }
+   function deposit() external payable {
+     balance += msg.value;
+   }
+   function withdraw(uint _amount) external {     
+     require(balance >= _amount, "Insufficient balance!");
+     payable(owner).transfer(_amount);
+     balance -= _amount;
+   }
+}
